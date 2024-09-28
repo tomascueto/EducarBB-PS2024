39cb3b61b6a770e770c63d525ed6e368a7b0ed94
NavarroRamiro14 <89555192+NavarroRamiro14@users.noreply.github.com>
2024-09-28 17:13:51 -0300

    Children se pasa como una prop a DashboardLayoutComponent en layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <DashboardLayoutComponent>{children}</DashboardLayoutComponent>
+        <DashboardLayoutComponent children={children}></DashboardLayoutComponent>
       </body>
     </html>
   );
