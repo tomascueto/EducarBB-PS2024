38e8da851a343040d2ccd511ba986893024255b7
Santi <santimaszong@gmail.com>
2024-10-18 19:00:35 -0300

    Fixeado Crear Plan de Estudio

--- app/(dashboard)/gestion-planes/page.tsx
+++ app/(dashboard)/gestion-planes/page.tsx
@@ -1,34 +1,38 @@
 
 import StudyPlanList from '@/components/studyplan-list';
-import { Materia, PlanEstudio } from '@/lib/definitions';
+import { fetchPlanes } from '@/lib/data';
+import { PlanEstudio } from '@/lib/definitions';
 
 export default async function AdminDashboard() {
 
+
+    const planes: PlanEstudio[] = await fetchPlanes();
+    
     // Lista provisoria de planes
-    const planes: PlanEstudio[] = [
-    {
-        "nombre": "Ingeniería en Sistemas de Información",
-        "materias": [
-            { "materia": { codigo: 'MAT001', nombre: 'Matemáticas', plan: undefined }, "año": "1" },
-            { "materia": { codigo: 'LFA101', nombre: 'Lenguajes Formáles y Autómatas', plan: undefined }, "año": "2" },
-            { "materia": { codigo: 'FIS102', nombre: 'Física I', plan: undefined }, "año": "3" },
-            { "materia": { codigo: 'BD200', nombre: 'Bases de Datos', plan: undefined }, "año": "4" }
-        ]
-    },
-    {
-        "nombre": "Licenciatura en Matemática",
-        "materias": [
-            { "materia": { codigo: 'MAT001', nombre: 'Matemáticas', plan: undefined }, "año": "1" }
-        ]
-    },
-    {
-        "nombre": "Licenciatura en Física",
-        "materias": [
-            { "materia": { codigo: 'MAT001', nombre: 'Matemáticas', plan: undefined }, "año": "1" },
-            { "materia": { codigo: 'FIS102', nombre: 'Física I', plan: undefined }, "año": "1" },
-        ]
-    }
-    ]
+    // const planes: PlanEstudio[] = [
+    // {
+    //     "nombre": "Ingeniería en Sistemas de Información",
+    //     "materias": [
+    //         { "materia": { codigo: 'MAT001', nombre: 'Matemáticas', plan: undefined }, "año": "1" },
+    //         { "materia": { codigo: 'LFA101', nombre: 'Lenguajes Formáles y Autómatas', plan: undefined }, "año": "2" },
+    //         { "materia": { codigo: 'FIS102', nombre: 'Física I', plan: undefined }, "año": "3" },
+    //         { "materia": { codigo: 'BD200', nombre: 'Bases de Datos', plan: undefined }, "año": "4" }
+    //     ]
+    // },
+    // {
+    //     "nombre": "Licenciatura en Matemática",
+    //     "materias": [
+    //         { "materia": { codigo: 'MAT001', nombre: 'Matemáticas', plan: undefined }, "año": "1" }
+    //     ]
+    // },
+    // {
+    //     "nombre": "Licenciatura en Física",
+    //     "materias": [
+    //         { "materia": { codigo: 'MAT001', nombre: 'Matemáticas', plan: undefined }, "año": "1" },
+    //         { "materia": { codigo: 'FIS102', nombre: 'Física I', plan: undefined }, "año": "1" },
+    //     ]
+    // }
+    // ]
 
     return(
         <>
