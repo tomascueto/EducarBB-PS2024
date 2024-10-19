e8c04aefed1c2aea942c1f8d444838f3d539e52d
NavarroRamiro14 <89555192+NavarroRamiro14@users.noreply.github.com>
2024-10-19 14:33:47 -0300

    CLEAN CODE

--- app/(dashboard)/gestion-materias/page.tsx
+++ app/(dashboard)/gestion-materias/page.tsx
@@ -6,15 +6,6 @@ import { Materia } from '@/lib/definitions';
 export default async function AdminDashboard() {
     
     const materias: Materia[] = await fetchMaterias();
- 
-
-    // Lista provisoria de materias
-    // const materias: Materia[] = [
-    //     { codigo: 'MAT001', nombre: 'Matemáticas', plan: undefined },
-    //     { codigo: 'LFA101', nombre: 'Lenguajes Formáles y Autómatas', plan: undefined },
-    //     { codigo: 'FIS102', nombre: 'Física I', plan: undefined },
-    //     { codigo: 'BD200', nombre: 'Bases de Datos', plan: undefined },
-    //   ];
     
     return(
         <>
