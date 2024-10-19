9ec2aadc0bd34f45ca4e101654ddb9eeca5fa2ed
NavarroRamiro14 <89555192+NavarroRamiro14@users.noreply.github.com>
2024-10-19 16:20:39 -0300

    borre el punto y coma en el titulo de gestion de aulas

--- app/(dashboard)/gestion-aulas/page.tsx
+++ app/(dashboard)/gestion-aulas/page.tsx
@@ -24,7 +24,7 @@ export default async function GestionAulas() {
     ]
 
     return<>
-        <div>Gestion de Aulas</div>;
+        <div>Gestion de Aulas</div>
         <ClassRoomList aulas={aulas} />
     </>
 }
