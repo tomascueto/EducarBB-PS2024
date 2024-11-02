fcff047b09c5b68dd9273f1c015751795f63b34e
NavarroRamiro14 <89555192+NavarroRamiro14@users.noreply.github.com>
2024-11-02 16:30:40 -0300

    botones de la pagina del aula

--- app/(dashboard)/gestion-aulas/[id-aula]/page.tsx
+++ app/(dashboard)/gestion-aulas/[id-aula]/page.tsx
@@ -15,5 +15,20 @@ export default async function AulaPage({ params }: { params: { 'id-aula': string
             <div>Materia: {aula.materia}</div>
         </div>
         <div>{JSON.stringify(aula)}</div>
+
+        <div className="flex items-center justify-center space-x-4 mt-4">
+            <a href="#" className="px-4 py-2 bg-blue-500 hover:bg-blue-700 text-white font-bold rounded">
+                Examenes
+            </a>
+            <a href="#" className="px-4 py-2 bg-green-500 hover:bg-green-700 text-white font-bold rounded">
+                Trabajos Practicos
+            </a>
+            <a href="#" className="px-4 py-2 bg-yellow-500 hover:bg-yellow-700 text-white font-bold rounded">
+                Asistencias
+            </a>
+            <a href="#" className="px-4 py-2 bg-red-500 hover:bg-red-700 text-white font-bold rounded">
+                Amonestaciones
+            </a>
+        </div>
     </>)
 }
